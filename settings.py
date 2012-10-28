6671d41eab41f6554bfaf7aad9b2ac7a97bc85d7
samuele.mattiuzzo <samumatt@gmail.com>
2012-10-28 02:06:19 +0100

    Align script: now with cloning and copying. Renamed app to blog

--- settings.py
+++ settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = (
 
     # djangoappengine should come last, so it can override a few manage.py commands
     'djangoappengine',
-    #'posta-to.core',
+    #'posta-to.blog',
     #'posta-to.adminpanel',
 )
 
--- srv/align_libs.py
+++ srv/align_libs.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python
-
-'''
-	Align script to copy over the folders needed
-'''
-
+from subprocess import call
 import os, shutil
-import sys
 
-BASE_PATH = "/home/smau/Workspace/GitHub/DJGAE" # basefolder for django appengine libraries
-PROJECT_PATH = ".."		 # project folder
-FOLDERS = [			 # libraries (comment do avoid copying)
+"""
+	ALIGN LIBS SCRIPT:
+
+	Requirements:
+	- git
+	- mercurial
+	- python
+	
+	This script automatically clones (either with git or hg) the requirements inside a
+	temporary folder and copies the libraries into the main application folder.
+	Cleans up at the end and removes the tmp/ directory.
+"""
+
+
+BASE_PATH = "tmp"   # temporary folder for cloning and copying
+PROJECT_PATH = ".." # project folder
+
+
+## Libs folders (comment to avoid copying)
+FOLDERS = [
 	("djangoappengine/djangoappengine", "djangoappengine"),
 	("django-nonrel/django", "django"),
 	("djangotoolbox/djangotoolbox", "djangotoolbox"),
@@ -17,14 +29,41 @@ FOLDERS = [			 # libraries (comment do avoid copying)
 	("django-dbindexer/dbindexer", "dbindexer"),
 ]
 
-
+## Repositories (with type associated)
+REPOS = [
+	("git", "https://github.com/django-nonrel/django-nonrel.git"),
+	("git", "https://github.com/django-nonrel/djangoappengine.git"),
+	("git", "https://github.com/django-nonrel/djangotoolbox.git"),
+	("hg", "https://bitbucket.org/twanschik/django-autoload"),
+	("git", "https://github.com/django-nonrel/django-dbindexer.git"),
+	#("git", "https://github.com/django-nonrel/django-testapp.git") # official test application
+]
 
 def main():
+
+	# creating temp directory
+	if not os.path.exists(BASE_PATH):
+    		os.makedirs(BASE_PATH)
+
+	os.chdir(BASE_PATH)
+
+	# cloning process
+	for r in REPOS:
+		call([r[0], "clone", r[1]])		
+	
+	os.chdir("..")
+
+	# copying the needed libs
 	for f in FOLDERS:
-		shutil.copytree(
-			os.sep.join([BASE_PATH, f[0]]) ,			# source
-			os.sep.join([os.getcwd(), PROJECT_PATH, f[1]])	# destination
-		)
+		start = os.sep.join([BASE_PATH, f[0]])
+		dest = os.sep.join([os.getcwd(), PROJECT_PATH, f[1]])
+		
+		if not os.path.exists(dest):
+			shutil.copytree(start, dest)
+
+	# removing temp directory
+	if os.path.isdir(BASE_PATH):
+        	shutil.rmtree(BASE_PATH)
 
 
 if __name__=="__main__":
